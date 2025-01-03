f2ad19a5873a02560ca4349e4d8291f867ab566d
leonardo-luz <leonardoluz.f02@gmail.com>
2025-01-03 04:59:32 -0300

    [feat] added random lorem ipsum words

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-## type.nvim
+## type.nvim [WIP]
 
 **Dependencies:** This plugin require `leonardo-luz/floatwindow`
 
 **Installation:**  Add `leonardo-luz/type.nvim` to your Neovim plugin manager (e.g., `init.lua` or `plugins/type.lua`).
 
 **Usage:**
-
-Type `:Type` to start
+* Type `:Type` to start
+    * `<bs>` back one word
+    * `<space>` advance one word
--- lua/type.lua
+++ lua/type.lua
@@ -1,52 +1,194 @@
 local questionnaire = require("templates.questionnaire")
-local ai = require("textgen")
 
 local M = {}
 
 local state = {
   streak = 0,
-  correct_answears = 0,
-  wrong_answears = 0,
+  correct_words = 0,
+  wrong_words = 0,
+  current_word = 1, -- All words
+  total_words = 1, -- All words
+  words = 20, -- All words
+  text = {},
+  start_timer = 0,
+  end_timer = 0,
+  wpm = 0,
 }
 
-M.setup_type = function()
-  local text = ai.generate_text({
-    prompt = "Generate a medium size text with a random size of a range of 8 to 16, with random words related to code and programming, send just the text.",
-  }).generated_text
+local lorem_ipsum = {
+  "lorem",
+  "ipsum",
+  "dolor",
+  "sit",
+  "amet",
+  "consectetur",
+  "adipiscing",
+  "elit",
+  "sed",
+  "do",
+  "eiusmod",
+  "tempor",
+  "incididunt",
+  "ut",
+  "labore",
+  "et",
+  "dolore",
+  "magna",
+  "aliqua",
+  "ut",
+  "enim",
+  "ad",
+  "minim",
+  "veniam",
+  "quis",
+  "nostrud",
+  "exercitation",
+  "ullamco",
+  "laboris",
+  "nisi",
+  "ut",
+  "aliquip",
+  "ex",
+  "ea",
+  "commodo",
+  "consequat",
+  "duis",
+  "aute",
+  "irure",
+  "dolor",
+  "in",
+  "reprehenderit",
+  "in",
+  "voluptate",
+  "velit",
+  "esse",
+  "cillum",
+  "dolore",
+  "eu",
+  "fugiat",
+  "nulla",
+  "pariatur",
+  "excepteur",
+  "sint",
+  "occaecat",
+  "cupidatat",
+  "non",
+  "proident",
+  "sunt",
+  "in",
+  "culpa",
+  "qui",
+  "officia",
+  "deserunt",
+  "mollit",
+  "anim",
+  "id",
+  "est",
+  "laborum",
+}
+
+local function generate_lorem_ipsum(word_count)
+  local text = {}
+  for i = 1, word_count do
+    local random_index = math.random(1, #lorem_ipsum)
+    table.insert(text, lorem_ipsum[random_index])
+    table.insert(state.text, lorem_ipsum[random_index])
+
+    if i % 10 == 0 then
+      table.insert(text, "\n")
+    end
+  end
+  return table.concat(text, " ")
+end
+
+local setup_type = function()
+  state.text = {}
+
+  local text = generate_lorem_ipsum(state.words)
 
   local lines = vim.split(text, "\n")
 
-  local footer =
-    string.format("  🔥 %d   |   %d ✔ / %d ✘", state.streak, state.correct_answears, state.wrong_answears)
+  local footer = string.format(
+    "  current word: %s   |   wpm  %d   |   ✔ %d / ✘ %d   |   🔥  %d  ",
+    state.text[state.current_word],
+    state.wpm,
+    state.streak,
+    state.correct_words,
+    state.wrong_words
+  )
 
   vim.api.nvim_buf_set_lines(questionnaire.state.window_style.footer.floating.buf, 0, -1, false, { footer })
 
   vim.api.nvim_buf_set_lines(questionnaire.state.window_style.answear.floating.buf, 0, -1, true, {})
+
+  vim.api.nvim_buf_set_lines(questionnaire.state.window_style.question.floating.buf, 0, -1, true, {})
   vim.api.nvim_buf_set_lines(questionnaire.state.window_style.question.floating.buf, 0, -1, true, lines)
 end
 
 local start_type = function()
+  math.randomseed(os.time())
+
+  state.start_timer = os.time()
+
   questionnaire.state.title = "Type Test"
   questionnaire.window_setup()
 
-  M.setup_type()
+  setup_type()
 
-  vim.keymap.set("n", "<cr>", function()
+  vim.keymap.set("n", "<bs>", function()
+    if state.current_word > 1 then
+      state.current_word = state.current_word - 1
+
+      local footer = string.format(
+        "  current word: %s   |   wpm  %d   |   ✔ %d / ✘ %d   |   🔥  %d  ",
+        state.text[state.current_word],
+        state.wpm,
+        state.correct_words,
+        state.wrong_words,
+        state.streak
+      )
+      vim.api.nvim_buf_set_lines(questionnaire.state.window_style.footer.floating.buf, 0, -1, false, { footer })
+    end
+  end, { buffer = questionnaire.state.window_style.answear.floating.buf })
+
+  vim.keymap.set("i", "<space>", function()
     local answear = vim.api.nvim_buf_get_lines(questionnaire.state.window_style.answear.floating.buf, 0, -1, true)
-    local question = vim.api.nvim_buf_get_lines(questionnaire.state.window_style.question.floating.buf, 0, -1, true)
 
-    local a = table.concat(answear, "\n")
-    local b = table.concat(question, "\n")
+    local answear_word = answear[1]
+    local target_word = state.text[state.current_word]
 
-    if a == b then
+    state.end_timer = os.time()
+
+    state.wpm = state.total_words / (state.end_timer - state.start_timer) * 60
+
+    if answear_word == target_word then
       state.streak = state.streak + 1
-      state.correct_answears = state.correct_answears + 1
+      state.correct_words = state.correct_words + 1
     else
       state.streak = 0
-      state.wrong_answears = state.wrong_answears + 1
+      state.wrong_words = state.wrong_words + 1
     end
 
-    M.setup_type()
+    if state.current_word >= state.words then
+      state.current_word = 1
+      setup_type()
+      return
+    end
+
+    state.current_word = state.current_word + 1
+    state.total_words = state.total_words + 1
+
+    vim.api.nvim_buf_set_lines(questionnaire.state.window_style.answear.floating.buf, 0, -1, true, {})
+
+    local footer = string.format(
+      "  current word: %s   |   wpm  %d   |   ✔ %d / ✘ %d   |   🔥  %d  ",
+      state.text[state.current_word],
+      state.wpm,
+      state.correct_words,
+      state.wrong_words,
+      state.streak
+    )
+    vim.api.nvim_buf_set_lines(questionnaire.state.window_style.footer.floating.buf, 0, -1, false, { footer })
   end, { buffer = questionnaire.state.window_style.answear.floating.buf })
 end
 
